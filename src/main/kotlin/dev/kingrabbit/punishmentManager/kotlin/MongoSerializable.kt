d90b8b89ea294a06e72b3b9f6ebc959b12fd4681
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2024-12-06 16:55:38 +1000

    Use single gson instance

--- src/main/kotlin/dev/kingrabbit/punishmentManager/kotlin/MongoSerializable.kt
+++ src/main/kotlin/dev/kingrabbit/punishmentManager/kotlin/MongoSerializable.kt
@@ -1,21 +1,19 @@
 package dev.kingrabbit.punishmentManager.kotlin
 
-import com.google.gson.Gson
+import gg.flyte.twilight.gson.GSON
 import org.bson.Document
 
 interface MongoSerializable {
 
     fun toDocument(): Document {
-        val gson = Gson()
-        return Document.parse(gson.toJson(this))
+        return Document.parse(GSON.toJson(this))
     }
 
     companion object {
         inline fun <reified T> fromDocument(document: Document?): T? {
             if (document == null) return null
-            val gson = Gson()
             val json = document.toJson()
-            return gson.fromJson(json, T::class.java)
+            return GSON.fromJson(json, T::class.java)
         }
     }
 
